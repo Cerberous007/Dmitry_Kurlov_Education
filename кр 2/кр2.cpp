2d08c7efe862e90bb10b927858fe808627c88fa1
Cerberous <kurlov_99@mail.ru>
2017-12-14 23:21:47 +0300

    14.12.2017
    Вторая задача из кр 14.12.2017
    Я сделяль

---
+++ кр 2/кр2.cpp
@@ -0,0 +1,69 @@
+// кр2.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+
+
+int decomp(int x)
+{
+	int sum = 0, j=0;
+	int *optDecomp = new int[x + 6];
+	int *cube_4 = new int[x + 2];	
+	for (; j*j*j+4 <= x; j++)
+	{
+		cube_4[j] = j*j*j+4;
+	}	
+	cube_4[j] = j*j*j + 4;
+	//memset(optDecomp, 127, sizeof optDecomp);
+	optDecomp[0] = 0;
+	optDecomp[1] = -1;
+	optDecomp[2] = -1;
+	optDecomp[3] = -1;
+	optDecomp[4] = 1;
+	optDecomp[5] = 1;
+	for (int i = 6; i <= x; i++)
+		optDecomp[i] = x;
+	for (int i = 6; i <= x; i++)
+	{
+		bool flag = true;
+		for (int k = 0; i >= cube_4[k]; k++)
+		{
+			if ((optDecomp[i] > optDecomp[i - cube_4[k]] + 1)&&optDecomp[i - cube_4[k]]!=-1)
+			{
+				optDecomp[i] = optDecomp[i - cube_4[k]] + 1;
+				flag = false;
+			}
+		}
+		if (flag)
+			optDecomp[i] = -1;
+	}
+	for (int i = x; i > 0;)
+	{
+		for (int k = 0; i >= cube_4[k]; k++)
+		{
+			if (optDecomp[i - cube_4[k]] == optDecomp[i] - 1)
+			{
+				printf("%d\n", cube_4[k]);
+				sum += k;
+				i -= cube_4[k];
+			}
+		}
+	}
+	delete[] optDecomp;
+	delete[] cube_4;
+	return sum;
+}
+
+
+
+int main()
+{
+	int a;
+	printf("Number >0\n");
+	scanf_s("%d", &a);
+	a = decomp(a);
+	printf("%d sum ai\n", a);
+
+	return 0;
+}
+
