415f889e5a2bb3d21f96faa6fa742172fa8ea3a1
Cerberous <kurlov_99@mail.ru>
2018-06-10 21:16:06 +0300

    new folder for 2 sem and solve matrix

---
+++ Second_sem/01.03.2018 Quaternions/quaternions/quaternions.h
@@ -0,0 +1,14 @@
+class quaternions
+{
+	private:
+	double a, b, c, d;
+	public:
+	quaternions() : a(0), b(0), c(0), d(0) {}
+	quaternions(double _a, double _b, double _c, double _d) : a(_a), b(_b), c(_c), d(_d) {}
+	quaternions &operator+=(const quaternions &x);
+	quaternions &operator*=(const quaternions &x);
+	quaternions &operator*=(const double x);	
+	quaternions &operator-=(const quaternions &x);
+	double abs();	
+	void print(const char msg[]);
+};
